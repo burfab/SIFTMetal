747565835f6887650ee94bf5f934ec32f072eeb3
Burgmann Fabian <33555831+burfab@users.noreply.github.com>
2026-04-20 22:13:23 +0200

    Add maskTexture to encode function in SIFTExtremaKernel
    Added maskTexture parameter to encode function and updated related preconditions and settings.

--- Sources/SIFTMetal/Metal Compute/SIFTExtremaKernel.swift
+++ Sources/SIFTMetal/Metal Compute/SIFTExtremaKernel.swift
@@ -27,6 +27,7 @@ final class SIFTExtremaFunction {
     func encode(
         commandBuffer: MTLCommandBuffer,
         inputTexture: MTLTexture,
+        maskTexture: MTLTexture,
         outputTexture: MTLTexture
     ) {
         precondition(inputTexture.width == outputTexture.width)
@@ -36,12 +37,20 @@ final class SIFTExtremaFunction {
         precondition(inputTexture.pixelFormat == .r32Float)
         precondition(outputTexture.textureType == .type2DArray)
         precondition(outputTexture.pixelFormat == .rg32Float)
+
+        let maskScaleX = Float(maskTexture.width) / Float(inputTexture.width)
+        let maskScaleY = Float(maskTexture.height) / Float(inputTexture.height)
+        
         
         let encoder = commandBuffer.makeComputeCommandEncoder()!
         encoder.label = "siftExtremaFunctionComputeEncoder"
         encoder.setComputePipelineState(computePipelineState)
         encoder.setTexture(outputTexture, index: 0)
         encoder.setTexture(inputTexture, index: 1)
+        encoder.setTexture(maskTexture, index: 2)
+        encoder.setFloat(maskScaleX, index: 0)
+        encoder.setFloat(maskScaleY, index: 1)
+
 
         let threadsPerDimension = Int(cbrt(Float(computePipelineState.maxTotalThreadsPerThreadgroup)))
         let threadsPerThreadgroup = MTLSize(
